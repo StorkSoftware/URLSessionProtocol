05219e824a36fef4e3e86bd883167720417cd667
Eimantas Vaičiūnas <eimantas@stork.software>
2018-01-13 19:33:53 +0200

    Initial Commit

---
+++ URLSessionProtocol/URLSessionProtocol.h
@@ -0,0 +1,19 @@
+//
+//  URLSessionProtocol.h
+//  URLSessionProtocol
+//
+//  Created by Eimantas Vaiciunas on 13/01/2018.
+//  Copyright © 2018 Stork Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for URLSessionProtocol.
+FOUNDATION_EXPORT double URLSessionProtocolVersionNumber;
+
+//! Project version string for URLSessionProtocol.
+FOUNDATION_EXPORT const unsigned char URLSessionProtocolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URLSessionProtocol/PublicHeader.h>
+
+
