56741b6ec2c980344bf1d9bddba36df1e09f366a
Eimantas Vaičiūnas <eimantas@stork.software>
2018-01-20 13:08:33 +0200

    Add more methods
    - add rest of the `URLSession` datatask methods;
    - make a public typealias for datatask completion handler;
    
    Signed-off-by: Eimantas Vaičiūnas <eimantas@stork.software>

--- URLSessionProtocol/URLSessionProtocol.swift
+++ URLSessionProtocol/URLSessionProtocol.swift
@@ -1,8 +1,13 @@
 import Foundation
 
+public typealias DataTaskHandler = (Data?, URLResponse?, Error?) -> Void
+
 public protocol URLSessionProtocol {
 
-    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol
+    func dataTask(with url: URL) -> URLSessionDataTaskProtocol
+    func dataTask(with url: URL, completionHandler: @escaping DataTaskHandler) -> URLSessionDataTaskProtocol
+    func dataTask(with request: URLRequest) -> URLSessionDataTaskProtocol
+    func dataTask(with request: URLRequest, completionHandler: @escaping DataTaskHandler) -> URLSessionDataTaskProtocol
 }
 
 public protocol URLSessionDataTaskProtocol {
@@ -17,7 +22,7 @@ extension URLSession: URLSessionProtocol {
         return ((dataTask(with: url) as URLSessionDataTask) as URLSessionDataTaskProtocol)
     }
 
-    public func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+    public func dataTask(with url: URL, completionHandler: @escaping DataTaskHandler) -> URLSessionDataTaskProtocol {
         return ((dataTask(with: url, completionHandler: completionHandler) as URLSessionDataTask) as URLSessionDataTaskProtocol)
     }
 
@@ -25,7 +30,7 @@ extension URLSession: URLSessionProtocol {
         return ((dataTask(with: request) as URLSessionDataTask) as URLSessionDataTaskProtocol)
     }
 
-    public func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+    public func dataTask(with request: URLRequest, completionHandler: @escaping DataTaskHandler) -> URLSessionDataTaskProtocol {
         return ((dataTask(with: request, completionHandler: completionHandler) as URLSessionDataTask) as URLSessionDataTaskProtocol)
     }
 }
