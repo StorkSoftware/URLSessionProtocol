e1f429d32c08a12e57c1e9d9a2e561988e951f8f
Eimantas Vaičiūnas <eimantas@stork.software>
2018-01-20 10:40:07 +0200

    Add testing facilities
    Signed-off-by: Eimantas Vaičiūnas <eimantas@stork.software>

---
+++ URLSessionProtocolTests/URLSessionProtocolSpec.swift
@@ -0,0 +1,11 @@
+import Quick
+import Nimble
+
+class URLSessionProtocolSpec: QuickSpec {
+
+    override func spec() {
+        it("should pass") {
+            expect(true).to(beTrue())
+        }
+    }
+}
