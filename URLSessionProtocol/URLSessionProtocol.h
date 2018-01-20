ed782ea19e7a168019599ea461bb3932ca29eaf8
Eimantas Vaičiūnas <eimantas@stork.software>
2018-01-20 09:19:42 +0200

    Setup project
    - create scheme for macOS
    - share both schemes
    
    Signed-off-by: Eimantas Vaičiūnas <eimantas@stork.software>

--- URLSessionProtocol/URLSessionProtocol.h
+++ URLSessionProtocol/URLSessionProtocol.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double URLSessionProtocolVersionNumber;
 FOUNDATION_EXPORT const unsigned char URLSessionProtocolVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <URLSessionProtocol/PublicHeader.h>
-
-
---
+++ URLSessionProtocol/URLSessionProtocol.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+protocol URLSessionProtocol {
+
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol
+}
+
+protocol URLSessionDataTaskProtocol {
+
+    func resume()
+    func cancel()
+}
+
+extension URLSession: URLSessionProtocol {
+
+    func dataTask(with url: URL) -> URLSessionDataTaskProtocol {
+        return ((dataTask(with: url) as URLSessionDataTask) as URLSessionDataTaskProtocol)
+    }
+
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+        return ((dataTask(with: url, completionHandler: completionHandler) as URLSessionDataTask) as URLSessionDataTaskProtocol)
+    }
+
+    func dataTask(with request: URLRequest) -> URLSessionDataTaskProtocol {
+        return ((dataTask(with: request) as URLSessionDataTask) as URLSessionDataTaskProtocol)
+    }
+
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+        return ((dataTask(with: request, completionHandler: completionHandler) as URLSessionDataTask) as URLSessionDataTaskProtocol)
+    }
+}
+
+extension URLSessionDataTask: URLSessionDataTaskProtocol { }
